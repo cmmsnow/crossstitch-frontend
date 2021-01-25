6c92ded7381bf1140701339162d7b7851b9d9b32
christine <christamsnow@gmail.com>
2021-01-24 22:29:40 -0500

    added first component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllColorsDisplayComponent } from './all-colors-display/all-colors-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllColorsDisplayComponent
   ],
   imports: [
     BrowserModule,
