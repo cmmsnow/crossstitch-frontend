c22ad1bda5c92746dae13632795222f994f8af20
christine <christamsnow@gmail.com>
2021-01-24 22:54:49 -0500

    threadColor service GET finished

---
+++ src/app/ThreadColor.ts
@@ -0,0 +1,6 @@
+export interface ThreadColor {
+  colorID: number;
+  colorName: string;
+  hexadecimal: string;
+  colorFamily: string;
+}
--- src/app/services/thread-color.service.ts
+++ src/app/services/thread-color.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ThreadColor} from '../ThreadColor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThreadColorService {
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
+  constructor(private http: HttpClient) { }
+
+  /** Save selected ThreadColor from the components */
+
+
+  /** GET ThreadColors from the server */
+  getComments(): Observable<ThreadColor[]> {
+    return this.http.get<ThreadColor[]>(`http://localhost:8080/threads/allthreads`);
+  }
+
 }
