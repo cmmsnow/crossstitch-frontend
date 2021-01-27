c238621eaa0a0e36393f415fc6641fb3b67518c8
christine <christamsnow@gmail.com>
2021-01-27 12:27:54 -0500

    app routing module hooked up and working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AllColorsDisplayComponent} from './components/all-colors-display/all-colors-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: AllColorsDisplayComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
